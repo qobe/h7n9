ce2272164c144b2f11476481209e825db26633cd
wombatatat <max.kobe@outlook.com>
2015-05-07 15:01:55 -0400

    libGDX project build

---
+++ android/src/kobe/games/h7n9/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package kobe.games.h7n9.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import kobe.games.h7n9.H7N9Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new H7N9Game(), config);
+	}
+}
---
+++ desktop/src/kobe/games/h7n9/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kobe.games.h7n9.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kobe.games.h7n9.H7N9Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new H7N9Game(), config);
+	}
+}
